9ca9a5d1010ff10efe41841c8b3cc8b9974d5cdd
John Wright <johngeorge.wright@gmail.com>
2016-09-28 10:28:57 +0100

    Local modules

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const functions = require('lodash.functionsin')
+const pathHelper = require('path')
+
+const isLocalModule = path => path.startsWith('.') || pathHelper.isAbsolute(path)
 
 const promisifyFunction = (fn, context) =>
   (...args) => new Promise((resolve, reject) => {
@@ -21,12 +24,23 @@ const promisifyObject = obj => functions(obj).reduce(
   Object.assign({}, obj)
 )
 
+const promisifyModule = path => {
+  if (isLocalModule(path)) {
+    path = pathHelper.resolve(pathHelper.dirname(module.parent.filename), path)
+  }
+  const mod = require(path)
+  if (typeof mod === 'string') {
+    throw new Error(`Can't promisify module "${path}" as it's an incorrect type`)
+  }
+  return promisify(mod)
+}
+
 const promisify = (fnOrStringOrObject, context = null) => {
   const type = typeof fnOrStringOrObject
   switch (type) {
     case 'function': return promisifyFunction(fnOrStringOrObject, context)
     case 'object': return promisifyObject(fnOrStringOrObject)
-    case 'string': return promisify(require(fnOrStringOrObject))
+    case 'string': return promisifyModule(fnOrStringOrObject)
     default: throw new Error(`Don't know how to promisify a "${type}"`)
   }
 }
--- test/unit.js
+++ test/unit.js
@@ -13,9 +13,21 @@ suite('modules', () => {
   test('immutability', () => fs.should.not.equal(fsPromised))
 
   test('a promised function', () => (
-    fsPromised.readdir(path.join(__dirname, 'fixtures'))
+    fsPromised.readdir(path.join(__dirname, 'fixtures', 'promised-function'))
     .should.eventually.eql(['1.txt', '2.txt', '3.txt'])
   ))
+
+  test('that local modules are relative to the parent', () => (
+    promisify('./fixtures/modules/local').should.have.property('foo', 'bar')
+  ))
+
+  test('that modules that return strings throw an error', () => {
+    const absPath = path.resolve(__dirname, './fixtures/modules/string')
+    const fn = () => promisify('./fixtures/modules/string')
+    fn.should.throw(
+      `Can't promisify module "${absPath}" as it's an incorrect type`
+    )
+  })
 })
 
 suite('functions', () => {
